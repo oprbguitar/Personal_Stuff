b1fe7da2e27ac14d634dd2b643f743c8d4731853
Pierre RB <oprbguitar@gmail.com>
2013-05-17 23:30:46 -0500

    ruby

---
+++ myblog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myblog::Application.config.secret_token = '11206b1e5284f27221e6c7d1da62149a3bd33e457d3e72fd11618e3e38c30bd9ab043b8db3f9a01fef9e856720f10e0be7c737ac1227efd9bbbcc85ed2b120fa'
